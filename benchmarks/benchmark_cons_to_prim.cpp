b0022760f62143e70dec98f098ed68d5b5448210
Timothy <timothy.pearsonn1@gmail.com>
2026-04-07 16:10:08 +0200

    neglible difference cons_to_prim vect<->scalar

--- benchmarks/benchmark_cons_to_prim.cpp
+++ benchmarks/benchmark_cons_to_prim.cpp
@@ -4,11 +4,11 @@
 #include <cons_to_prim.hpp>
 #include <euler_arrays.hpp>
 #include <perfect_gas.hpp>
-#include "utils.hpp"
 
 #include "benchmark_utils.hpp"
 #include "index_type.hpp"
 #include "real_type.hpp"
+#include "utils.hpp"
 
 namespace {
 
@@ -40,7 +40,94 @@ void ConsToPrim(benchmark::State& state)
     set_constant_cells_processed(state, size(prim_arrays));
     set_constant_bytes_processed(state, size_bytes(prim_arrays) + size_bytes(cons_arrays));
 }
+void ConsToPrimVectorized(benchmark::State& state)
+{
+    auto const n = int_cast<index_t>(state.range());
+    PerfectGas<real_t> const eos(1.4);
+    Kokkos::DefaultExecutionSpace const exec_space;
+    EulerPrimArrays const prims_alloc = create_prim_arrays_1d<real_t>(exec_space, n * n * n);
+    EulerPrimArrays const prim_arrays = to_mdspan<Kokkos::mdspan<
+            real_t,
+            Kokkos::dextents<index_t, 3>,
+            Kokkos::layout_left>>(prims_alloc, n, n, n);
+    EulerConsArrays const cons_alloc = create_cons_arrays_1d<real_t>(exec_space, n * n * n);
+    EulerConsArrays const cons_arrays = to_mdspan<Kokkos::mdspan<
+            real_t,
+            Kokkos::dextents<index_t, 3>,
+            Kokkos::layout_left>>(cons_alloc, n, n, n);
+    EulerCons<real_t> const cons {.d = 1, .e = 1 / 0.4, .mx0 = 0, .mx1 = 0, .mx2 = 0};
+    init_from_state(exec_space, cons_arrays, cons);
+    exec_space.fence();
 
+    for ([[maybe_unused]] auto _ : state) {
+        cons_to_prim_vec(exec_space, as_const(cons_arrays), prim_arrays, eos);
+        exec_space.fence();
+        benchmark::ClobberMemory();
+    }
+
+    set_constant_cells_processed(state, size(prim_arrays));
+    set_constant_bytes_processed(state, size_bytes(prim_arrays) + size_bytes(cons_arrays));
+}
+
+void ConsToPrimWorstRem(benchmark::State& state)
+{
+    auto const n = int_cast<index_t>(state.range());
+    PerfectGas<real_t> const eos(1.4);
+    Kokkos::DefaultExecutionSpace const exec_space;
+    EulerPrimArrays const prims_alloc = create_prim_arrays_1d<real_t>(exec_space, n * n * n);
+    EulerPrimArrays const prim_arrays = to_mdspan<Kokkos::mdspan<
+            real_t,
+            Kokkos::dextents<index_t, 3>,
+            Kokkos::layout_left>>(prims_alloc, n, n, n);
+    EulerConsArrays const cons_alloc = create_cons_arrays_1d<real_t>(exec_space, n * n * n);
+    EulerConsArrays const cons_arrays = to_mdspan<Kokkos::mdspan<
+            real_t,
+            Kokkos::dextents<index_t, 3>,
+            Kokkos::layout_left>>(cons_alloc, n, n, n);
+    EulerCons<real_t> const cons {.d = 1, .e = 1 / 0.4, .mx0 = 0, .mx1 = 0, .mx2 = 0};
+    init_from_state(exec_space, cons_arrays, cons);
+    exec_space.fence();
+
+    for ([[maybe_unused]] auto _ : state) {
+        cons_to_prim_vec(exec_space, as_const(cons_arrays), prim_arrays, eos);
+        exec_space.fence();
+        benchmark::ClobberMemory();
+    }
+
+    set_constant_cells_processed(state, size(prim_arrays));
+    set_constant_bytes_processed(state, size_bytes(prim_arrays) + size_bytes(cons_arrays));
+}
+void ConsToPrimWorstRemVectorized(benchmark::State& state)
+{
+    auto const n = int_cast<index_t>(state.range());
+    PerfectGas<real_t> const eos(1.4);
+    Kokkos::DefaultExecutionSpace const exec_space;
+    EulerPrimArrays const prims_alloc = create_prim_arrays_1d<real_t>(exec_space, n * n * n);
+    EulerPrimArrays const prim_arrays = to_mdspan<Kokkos::mdspan<
+            real_t,
+            Kokkos::dextents<index_t, 3>,
+            Kokkos::layout_left>>(prims_alloc, n, n, n);
+    EulerConsArrays const cons_alloc = create_cons_arrays_1d<real_t>(exec_space, n * n * n);
+    EulerConsArrays const cons_arrays = to_mdspan<Kokkos::mdspan<
+            real_t,
+            Kokkos::dextents<index_t, 3>,
+            Kokkos::layout_left>>(cons_alloc, n, n, n);
+    EulerCons<real_t> const cons {.d = 1, .e = 1 / 0.4, .mx0 = 0, .mx1 = 0, .mx2 = 0};
+    init_from_state(exec_space, cons_arrays, cons);
+    exec_space.fence();
+
+    for ([[maybe_unused]] auto _ : state) {
+        cons_to_prim_vec(exec_space, as_const(cons_arrays), prim_arrays, eos);
+        exec_space.fence();
+        benchmark::ClobberMemory();
+    }
+
+    set_constant_cells_processed(state, size(prim_arrays));
+    set_constant_bytes_processed(state, size_bytes(prim_arrays) + size_bytes(cons_arrays));
+}
 } // namespace
 
-BENCHMARK(ConsToPrim)->DenseRange(8, 31, 8)->DenseRange(32, 320, 32);
+BENCHMARK(ConsToPrim)->DenseRange(8, 128, 8)->DenseRange(128, 320, 32);
+BENCHMARK(ConsToPrimVectorized)->DenseRange(8, 128, 8)->DenseRange(128, 320, 32);
+BENCHMARK(ConsToPrimWorstRem)->DenseRange(7, 128, 8)->DenseRange(127, 320, 32);
+BENCHMARK(ConsToPrimWorstRemVectorized)->DenseRange(7, 128, 8)->DenseRange(127, 320, 32);
