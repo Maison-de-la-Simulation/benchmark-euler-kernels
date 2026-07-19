43b6b82e4d647b02e5a9d013eef19a911ebaf41b
tpearson <timothy-edward.pearson@epita.fr>
2026-07-19 14:17:49 +0200

    manually unrolled godunov

--- euler_operators/godunov.hpp
+++ euler_operators/godunov.hpp
@@ -266,7 +266,7 @@ void godunov_kernel(
 }
 
 template <class T, class IndexType, std::size_t E0, std::size_t E1, std::size_t E2>
-void godunov_vec2(
+void godunov_vec(
         Kokkos::DefaultExecutionSpace const& exec_space,
         EulerPrimArrays<Kokkos::mdspan<
                 T const,
@@ -325,7 +325,7 @@ void godunov_vec2(
 }
 
 template <class T, class IndexType, std::size_t E0, std::size_t E1, std::size_t E2>
-void godunov_vec(
+void godunov_vec2(
         Kokkos::DefaultExecutionSpace const& exec_space,
         EulerPrimArrays<Kokkos::mdspan<
                 T const,
@@ -380,6 +380,231 @@ void godunov_vec(
         EulerConsArrays const sub_cons_arrays = subspan(cons_arrays, slice0, slice1, slice2);
 
         godunov_kernel<simd_scalar_t>(
+                exec_space,
+                sub_prim_arrays,
+                sub_cons_arrays,
+                eos,
+                mesh,
+                riemann_solver,
+                dt);
+    }
+}
+
+template <class T, class IndexType, std::size_t E0, std::size_t E1, std::size_t E2>
+void godunov_vec_unrolled(
+        Kokkos::DefaultExecutionSpace const& exec_space,
+        EulerPrimArrays<Kokkos::mdspan<
+                T const,
+                Kokkos::extents<IndexType, E0, E1, E2>,
+                Kokkos::layout_left>> const& prim_arrays,
+        EulerConsArrays<Kokkos::mdspan<
+                T,
+                Kokkos::extents<IndexType, E0, E1, E2>,
+                Kokkos::layout_left>> const& cons_arrays,
+        PerfectGas<T> const& eos,
+        UniformMesh3d<T> const& mesh,
+        hllc const& riemann_solver,
+        T const dt)
+{
+    namespace KE = Kokkos::Experimental;
+
+    using simd_t = KE::simd<T>;
+    using scalar_simd_t = KE::basic_simd<T, KE::simd_abi::scalar>;
+
+    constexpr IndexType simd_width = static_cast<IndexType>(simd_t::size());
+    constexpr IndexType unroll_factor = 2;
+    constexpr IndexType cells_per_iteration = unroll_factor * simd_width;
+
+    IndexType const n0 = static_cast<IndexType>(prim_arrays.d.extent(0));
+    IndexType const n0_begin = 1;
+    IndexType const n0_end = n0 - 1;
+    IndexType const n0_inner = n0 - 2;
+
+    IndexType const unrolled_cells = (n0_inner / cells_per_iteration) * cells_per_iteration;
+
+    IndexType const unrolled_end = n0_begin + unrolled_cells;
+
+    Kokkos::full_extent_t const slice1;
+    Kokkos::full_extent_t const slice2;
+
+    if (unrolled_cells > 0) {
+        Kokkos::pair const slice0(IndexType {0}, unrolled_end + IndexType {1});
+
+        EulerPrimArrays const sub_prim_arrays = subspan(prim_arrays, slice0, slice1, slice2);
+
+        EulerConsArrays const sub_cons_arrays = subspan(cons_arrays, slice0, slice1, slice2);
+
+        EulerPrimArrays<T const*> const prim_ptrs {
+                sub_prim_arrays.d.data_handle(),
+                sub_prim_arrays.p.data_handle(),
+                sub_prim_arrays.ux0.data_handle(),
+                sub_prim_arrays.ux1.data_handle(),
+                sub_prim_arrays.ux2.data_handle()};
+
+        EulerConsArrays<T*> const cons_ptrs {
+                sub_cons_arrays.d.data_handle(),
+                sub_cons_arrays.e.data_handle(),
+                sub_cons_arrays.mx0.data_handle(),
+                sub_cons_arrays.mx1.data_handle(),
+                sub_cons_arrays.mx2.data_handle()};
+
+        IndexType const extent1 = static_cast<IndexType>(sub_prim_arrays.d.extent(1));
+        IndexType const extent2 = static_cast<IndexType>(sub_prim_arrays.d.extent(2));
+
+        std::size_t const stride0 = sub_prim_arrays.d.stride(0);
+        std::size_t const stride1 = sub_prim_arrays.d.stride(1);
+        std::size_t const stride2 = sub_prim_arrays.d.stride(2);
+
+        IndexType const number_of_unrolled_blocks = unrolled_cells / cells_per_iteration;
+
+        simd_t const dt_dx(dt / mesh.dx0());
+        simd_t const dt_dy(dt / mesh.dx1());
+        simd_t const dt_dz(dt / mesh.dx2());
+
+        using policy_type = Kokkos::MDRangePolicy<Kokkos::DefaultExecutionSpace, Kokkos::Rank<3>>;
+
+        policy_type const
+                policy(exec_space,
+                       {IndexType {0}, IndexType {1}, IndexType {1}},
+                       {number_of_unrolled_blocks,
+                        extent1 - IndexType {1},
+                        extent2 - IndexType {1}});
+
+        Kokkos::parallel_for(
+                "godunov_vec_unrolled",
+                policy,
+                KOKKOS_LAMBDA(IndexType const block, IndexType const j, IndexType const k) {
+                    std::size_t const i0
+                            = static_cast<std::size_t>(IndexType {1} + block * cells_per_iteration);
+
+                    std::size_t const base0 = i0 * stride0 + static_cast<std::size_t>(j) * stride1
+                                              + static_cast<std::size_t>(k) * stride2;
+
+                    std::size_t const base1
+                            = base0 + static_cast<std::size_t>(simd_width) * stride0;
+
+                    {
+                        EulerPrim<simd_t> const prim = load<simd_t>(prim_ptrs, base0);
+
+                        EulerPrim<simd_t> const prim_xm = load<simd_t>(prim_ptrs, base0 - stride0);
+
+                        EulerPrim<simd_t> const prim_xp = load<simd_t>(prim_ptrs, base0 + stride0);
+
+                        EulerPrim<simd_t> const prim_ym = load<simd_t>(prim_ptrs, base0 - stride1);
+
+                        EulerPrim<simd_t> const prim_yp = load<simd_t>(prim_ptrs, base0 + stride1);
+
+                        EulerPrim<simd_t> const prim_zm = load<simd_t>(prim_ptrs, base0 - stride2);
+
+                        EulerPrim<simd_t> const prim_zp = load<simd_t>(prim_ptrs, base0 + stride2);
+
+                        EulerFlux<simd_t> const flux_xm
+                                = riemann_solver(dir_t<0>(), eos, prim_xm, prim);
+
+                        EulerFlux<simd_t> const flux_xp
+                                = riemann_solver(dir_t<0>(), eos, prim, prim_xp);
+
+                        EulerFlux<simd_t> const flux_ym
+                                = riemann_solver(dir_t<1>(), eos, prim_ym, prim);
+
+                        EulerFlux<simd_t> const flux_yp
+                                = riemann_solver(dir_t<1>(), eos, prim, prim_yp);
+
+                        EulerFlux<simd_t> const flux_zm
+                                = riemann_solver(dir_t<2>(), eos, prim_zm, prim);
+
+                        EulerFlux<simd_t> const flux_zp
+                                = riemann_solver(dir_t<2>(), eos, prim, prim_zp);
+
+                        EulerCons<simd_t> cons = load<simd_t>(cons_ptrs, base0);
+
+                        cons.d -= dt_dx * (flux_xp.d - flux_xm.d) + dt_dy * (flux_yp.d - flux_ym.d)
+                                  + dt_dz * (flux_zp.d - flux_zm.d);
+
+                        cons.e -= dt_dx * (flux_xp.e - flux_xm.e) + dt_dy * (flux_yp.e - flux_ym.e)
+                                  + dt_dz * (flux_zp.e - flux_zm.e);
+
+                        cons.mx0 -= dt_dx * (flux_xp.mx0 - flux_xm.mx0)
+                                    + dt_dy * (flux_yp.mx0 - flux_ym.mx0)
+                                    + dt_dz * (flux_zp.mx0 - flux_zm.mx0);
+
+                        cons.mx1 -= dt_dx * (flux_xp.mx1 - flux_xm.mx1)
+                                    + dt_dy * (flux_yp.mx1 - flux_ym.mx1)
+                                    + dt_dz * (flux_zp.mx1 - flux_zm.mx1);
+
+                        cons.mx2 -= dt_dx * (flux_xp.mx2 - flux_xm.mx2)
+                                    + dt_dy * (flux_yp.mx2 - flux_ym.mx2)
+                                    + dt_dz * (flux_zp.mx2 - flux_zm.mx2);
+
+                        store<simd_t>(cons, cons_ptrs, base0);
+                    }
+
+                    {
+                        EulerPrim<simd_t> const prim = load<simd_t>(prim_ptrs, base1);
+
+                        EulerPrim<simd_t> const prim_xm = load<simd_t>(prim_ptrs, base1 - stride0);
+
+                        EulerPrim<simd_t> const prim_xp = load<simd_t>(prim_ptrs, base1 + stride0);
+
+                        EulerPrim<simd_t> const prim_ym = load<simd_t>(prim_ptrs, base1 - stride1);
+
+                        EulerPrim<simd_t> const prim_yp = load<simd_t>(prim_ptrs, base1 + stride1);
+
+                        EulerPrim<simd_t> const prim_zm = load<simd_t>(prim_ptrs, base1 - stride2);
+
+                        EulerPrim<simd_t> const prim_zp = load<simd_t>(prim_ptrs, base1 + stride2);
+
+                        EulerFlux<simd_t> const flux_xm
+                                = riemann_solver(dir_t<0>(), eos, prim_xm, prim);
+
+                        EulerFlux<simd_t> const flux_xp
+                                = riemann_solver(dir_t<0>(), eos, prim, prim_xp);
+
+                        EulerFlux<simd_t> const flux_ym
+                                = riemann_solver(dir_t<1>(), eos, prim_ym, prim);
+
+                        EulerFlux<simd_t> const flux_yp
+                                = riemann_solver(dir_t<1>(), eos, prim, prim_yp);
+
+                        EulerFlux<simd_t> const flux_zm
+                                = riemann_solver(dir_t<2>(), eos, prim_zm, prim);
+
+                        EulerFlux<simd_t> const flux_zp
+                                = riemann_solver(dir_t<2>(), eos, prim, prim_zp);
+
+                        EulerCons<simd_t> cons = load<simd_t>(cons_ptrs, base1);
+
+                        cons.d -= dt_dx * (flux_xp.d - flux_xm.d) + dt_dy * (flux_yp.d - flux_ym.d)
+                                  + dt_dz * (flux_zp.d - flux_zm.d);
+
+                        cons.e -= dt_dx * (flux_xp.e - flux_xm.e) + dt_dy * (flux_yp.e - flux_ym.e)
+                                  + dt_dz * (flux_zp.e - flux_zm.e);
+
+                        cons.mx0 -= dt_dx * (flux_xp.mx0 - flux_xm.mx0)
+                                    + dt_dy * (flux_yp.mx0 - flux_ym.mx0)
+                                    + dt_dz * (flux_zp.mx0 - flux_zm.mx0);
+
+                        cons.mx1 -= dt_dx * (flux_xp.mx1 - flux_xm.mx1)
+                                    + dt_dy * (flux_yp.mx1 - flux_ym.mx1)
+                                    + dt_dz * (flux_zp.mx1 - flux_zm.mx1);
+
+                        cons.mx2 -= dt_dx * (flux_xp.mx2 - flux_xm.mx2)
+                                    + dt_dy * (flux_yp.mx2 - flux_ym.mx2)
+                                    + dt_dz * (flux_zp.mx2 - flux_zm.mx2);
+
+                        store<simd_t>(cons, cons_ptrs, base1);
+                    }
+                });
+    }
+
+    if (unrolled_end < n0_end) {
+        Kokkos::pair const slice0(unrolled_end - IndexType {1}, n0);
+
+        EulerPrimArrays const sub_prim_arrays = subspan(prim_arrays, slice0, slice1, slice2);
+
+        EulerConsArrays const sub_cons_arrays = subspan(cons_arrays, slice0, slice1, slice2);
+
+        godunov_kernel<scalar_simd_t>(
                 exec_space,
                 sub_prim_arrays,
                 sub_cons_arrays,
